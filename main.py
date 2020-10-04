c72a27cabdcd308c4d6ca468f42e64d1f2051e6a
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-10-05 04:02:27 +0900

    Update def

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def handle_message(event):
     )
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='ジャンルは'+artist_items['genres'][0]+'だね')
+        TextSendMessage(text='ジャンルは'+'だね')
     )
 
 
