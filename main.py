41a71792ae3348223e33f7fc8b484425f600e556
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-10-05 03:39:02 +0900

    Add def

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from flask import Flask, request, abort
 import os
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 import spotipy
 import json
@@ -21,17 +15,23 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
-#環境変数取得
+# 環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+client_id = os.environ["client_id"]
+client_secret = os.environ["client_secret"]
+client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Webで開いたとき
 @app.route("/")
 def hello_world():
     return "hello world!"
 
+# LINEで開いたとき
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -49,11 +49,26 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+# 友だち追加時の関数
+@handler.add(FollowEvent)
+def handle_follow(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='好きなアーティスト名を教えてくれ')
+    )
+
+# テキストを送ったときの関数
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    txt = event.message.text
+    # artist_df = pd.DataFrame(
+    #     columns=['artist_name','artist_ID','genre','popularity','related_artist_names']
+    # )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=txt+'が好きなんだ~')
+    )
+
 
 if __name__ == "__main__":
 #    app.run()
