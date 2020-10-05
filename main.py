77d2f1eb4e9c69b5a34e1a9674d7f9ea8b165283
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-10-06 05:04:08 +0900

    Update fontprop

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import spotipy
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import networkx as nx
 from spotipy.oauth2 import SpotifyClientCredentials 
 
+fontprop = FontProperties(fname='static/fonts/NotoSansJP-Bold.otf', size=10)
 app = Flask(__name__)
 
 # 環境変数取得
@@ -115,9 +117,8 @@ def handle_message(event):
         def calc_inverse(n):
             return 1/n
 
-        font_path = "static/fonts/NotoSansJP-Bold.otf"
         nx.draw_networkx_nodes(G, pos, alpha=.6, node_color=list(map(calc_inverse,list(pr.values()))), cmap=plt.cm.GnBu, node_size=[200*(1/v) for v in pr.values()])
-        nx.draw_networkx_labels(G, pos, font_size=14, font_family=font_path, font_weight="bold")
+        nx.draw_networkx_labels(G, pos, font_size=14, font_family=fontprop, font_weight="bold")
         nx.draw_networkx_edges(G, pos, alpha=1, edge_color="c")
         plt.axis("off")
 
